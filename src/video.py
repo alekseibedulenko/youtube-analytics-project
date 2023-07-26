ef24ea73ccf8a4789f57ce11cc3a8181b86e434d
alekseii <dorogazm@gmail.com>
2023-07-26 17:09:51 +0300

    youtube-analytics hw-6.2

--- src/video.py
+++ src/video.py
@@ -4,14 +4,22 @@ from src.channel import Channel
 class Video(Channel):
     def __init__(self, video_id):
         self.video_id = video_id
-        self.youtube = self.get_service().videos().list(
-            part='snippet,statistics', id=self.video_id
-        ).execute()
-        self.video_data = self.youtube.get("items")[0]
-        self.title = self.video_data.get('snippet').get('title')
-        self.url = f'https://www.youtube.com/watch?v={self.video_id}'
-        self.view_count = int(self.video_data.get('statistics').get('viewCount'))
-        self.like_count = int(self.video_data.get('statistics').get('likeCount'))
+        try:
+            self.youtube = self.get_service().videos().list(
+                part='snippet,statistics', id=self.video_id
+            ).execute()
+            self.video_data = self.youtube.get("items")[0]
+        except IndexError as e:
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+            print(e)
+        else:
+            self.title = self.video_data.get('snippet').get('title')
+            self.url = f'https://www.youtube.com/watch?v={self.video_id}'
+            self.view_count = int(self.video_data.get('statistics').get('viewCount'))
+            self.like_count = int(self.video_data.get('statistics').get('likeCount'))
     def __str__(self):
         return self.title
 
